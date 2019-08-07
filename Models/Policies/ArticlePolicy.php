ca383f4b9be218465119c9060ebae50accd6e949
marco76tv <marco.sottana@gmail.com>
2019-08-07 22:49:27 +0200

    .

--- Models/Policies/ArticlePolicy.php
+++ Models/Policies/ArticlePolicy.php
@@ -1,90 +1,11 @@
 <?php
 namespace Modules\Blog\Models\Policies;
-
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Modules\Blog\Models\Article as Post;
 use Modules\LU\Models\User;
 
-class ArticlePolicy
-{
-    use HandlesAuthorization;
+use Modules\Xot\Models\Policies\XotBasePolicy;
 
-    public function before($user, $ability)
-    {
-        if (isset($user->perm) && $user->perm->perm_type >= 5) {  //superadmin
-            return true;
-        }
-        
-    }
+class ArticlePolicy extends XotBasePolicy{
 
-    /*
-    public function update(User $user, Post $post)
-    {
-        return $user->id === $post->user_id;
-    }
-    */
-    public function index(User $user, Post $post)
-    {
-        return true;
-    }
-
-    public function create(User $user, Post $post)
-    {
-        return true;
-    }
-
-    public function destroy(User $user, Post $post)
-    {
-        if ($post->created_by == $user->handle) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public function detach(User $user, Post $post)
-    {
-        if ($post->created_by == $user->handle) {
-            return true;
-        }
-
-        return false;
-    }
-    
-    public function edit(User $user, Post $post)
-    {
-        if ($post->created_by == $user->handle) {
-            return true;
-        }
-
-        return false;
-    }
-    public function update(User $user, Post $post)
-    {
-        if ($post->created_by == $user->handle) {
-            return true;
-        }
-
-        return false;
-    }
-    public function store(User $user, Post $post)
-    {
-        if ($post->created_by == $user->handle) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public function show(User $user, Post $post)
-    {
-        return false;
-    }
-
-    public function delete(User $user, Post $post){
-        return true;
-    }
-    public function indexEdit(User $user, Post $post){
-        return true;
-    }
 }
