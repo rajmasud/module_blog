695060630d75dd0da952f370f5fa4615d29cb80b
marco76tv <marco.sottana@gmail.com>
2019-10-16 23:04:33 +0200

    .

--- Models/Policies/MyRatingPolicy.php
+++ Models/Policies/MyRatingPolicy.php
@@ -5,4 +5,9 @@ namespace Modules\Blog\Models\Policies;
 use Modules\Xot\Models\Policies\XotBasePolicy;
 
 class MyRatingPolicy extends XotBasePolicy {
+
+	public function create($user,$post){
+		return false;
+	}
+
 }
