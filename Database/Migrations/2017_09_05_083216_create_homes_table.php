37ae76eabe3e93adf64522b29f9ad03b0ba58b6d
marco76tv <marco.sottana@gmail.com>
2020-01-28 13:58:47 +0100

    .

---
+++ Database/Migrations/2017_09_05_083216_create_homes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+//----- models -----
+use Modules\Blog\Models\Home as MyModel;
+
+class CreateHomesTable extends Migration
+{
+    public function getTable()
+    {
+        return with(new MyModel())->getTable();
+    }
+
+    public function up()
+    {
+        if (!Schema::hasTable($this->getTable())) {
+            Schema::create($this->getTable(), function (Blueprint $table) {
+                $table->increments('post_id'); //->primary();
+                $table->string('created_by')->nullable();
+                $table->string('updated_by')->nullable();
+                $table->timestamps();
+            });
+        }
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists($this->getTable());
+    }
+}
