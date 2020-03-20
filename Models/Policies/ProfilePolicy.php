1e3b0e258e2afd70265e18b322d18f8564eb19b6
Dade <davidecavallini1987@gmail.com>
2020-03-20 14:12:43 +0100

    up

--- Models/Policies/ProfilePolicy.php
+++ Models/Policies/ProfilePolicy.php
@@ -5,4 +5,9 @@ namespace Modules\Blog\Models\Policies;
 use Modules\Xot\Models\Policies\XotBasePolicy;
 
 class ProfilePolicy extends XotBasePolicy {
+    
+    public function create($user,$post){
+        return true;
+    }
+
 }
