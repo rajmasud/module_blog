0e6c54ee6b0fac50ee750a52cd5b563b70f58148
Malebestia <malebestia@yahoo.it>
2020-04-04 13:50:31 +0200

    .

--- Database/Migrations/2017_09_05_083216_create_homes_table.php
+++ Database/Migrations/2017_09_05_083216_create_homes_table.php
@@ -5,21 +5,33 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 //----- models -----
 use Modules\Blog\Models\Home as MyModel;
+//----- bases ----
+use Modules\Xot\Database\Migrations\XotBaseMigration;
 
-class CreateHomesTable extends Migration {
-    public function getTable() {
-        return with(new MyModel())->getTable();
-    }
+class CreateHomesTable extends XotBaseMigration {
+    
 
     public function up() {
-        if (! Schema::hasTable($this->getTable())) {
-            Schema::create($this->getTable(), function (Blueprint $table) {
-                $table->increments('post_id'); //->primary();
-                $table->string('created_by')->nullable();
-                $table->string('updated_by')->nullable();
-                $table->timestamps();
-            });
+        //-- CREATE --
+        if (! $this->tableExists()) {
+            $this->getConn()->create($this->getTable(), 
+                function (Blueprint $table) {
+                    $table->increments('post_id'); //->primary();
+                    $table->string('created_by')->nullable();
+                    $table->string('updated_by')->nullable();
+                    $table->timestamps();
+                }
+            );
         }
+        //-- UPDATE --
+        $this->getConn()->table($this->getTable(), 
+            function (Blueprint $table) {
+                if (! $this->hasColumn('icon_src')) {
+                    $table->string('icon_src')->nullable();
+                }
+            }
+        );
+
     }
 
     public function down() {
