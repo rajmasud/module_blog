339682af25f33d59ae20242dbe421c3033f15695
Malebestia <malebestia@yahoo.it>
2020-04-13 17:15:52 +0200

    .

---
+++ Models/Traits/AmenityTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Blog\Models\Traits;
+
+//use Laravel\Scout\Searchable;
+
+//----- models------
+use Modules\Blog\Models\Amenity;
+//---- services -----
+//use Modules\Xot\Services\PanelService as Panel;
+
+//------ traits ---
+
+trait AmenityTrait {
+
+    // elenco delle amenities collegati ad un determinato modello collegato (es: ristorante)
+    public function amenities() {
+        return $this->morphRelated(Amenity::class);
+    }
+
+    // elenco delle amenities totali (non collegate a nessuna istanza di modello collegato)
+    public function amenityObjectives() {
+        $related = Amenity::class;
+        return $this->hasMany($related, 'related_type', 'post_type');
+    }
+
+}
