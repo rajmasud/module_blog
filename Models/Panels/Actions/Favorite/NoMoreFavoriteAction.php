f89b11838afd156a7022fefe64ccb7dac4b12fd7
marco76tv <marco.sottana@gmail.com>
2020-04-16 16:58:33 +0200

    .

---
+++ Models/Panels/Actions/Favorite/NoMoreFavoriteAction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Modules\Blog\Models\Panels\Actions\Favorite;
+
+//-------- services --------
+use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
+
+//-------- bases -----------
+class NoMoreFavoriteAction extends XotBasePanelAction {
+    public $onContainer = true;
+    //public $onItem = true; //onlyContainer
+    //mettere freccette su e giù
+    public $icon = '<i class="fa fa-arrow-up"></i><i class="fa fa-arrow-down"></i>';
+
+    public $auth_user_id;
+
+    public function __construct($auth_user_id) {
+        $this->auth_user_id = $auth_user_id;
+    }
+
+    //-- Perform the action on the given models.
+    public function handle() {
+        //dddx($this);
+        //return 'ciao';
+        //ddd($this->row);
+        dddx($this->rows);
+    }
+
+    public function postHandle() {
+        //$this->rows->where('auth_user_id', $this->auth_user_id);
+        $route_params = \Route::current()->parameters();
+        [$containers,$items] = params2ContainerItem($route_params);
+        $func = 'favorites';
+        last($items)->$func()->where('auth_user_id', $this->auth_user_id)->delete();
+
+        return 'fatto';
+    }
+}
--- Models/Policies/FavoritePolicy.php
+++ Models/Policies/FavoritePolicy.php
@@ -3,9 +3,13 @@ namespace Modules\Blog\Models\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Modules\LU\Models\User as User;
-use Modules\Blog\Models\Favorite as Post; 
+use Modules\Blog\Models\Favorite as Post;
 
 use Modules\Xot\Models\Policies\XotBasePolicy;
 
 class FavoritePolicy extends XotBasePolicy {
+
+    function noMoreFavorite($user,$post){
+        return true;
+    }
 }
