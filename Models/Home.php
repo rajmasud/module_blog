2608f7093d215a91220deeb4a66710f90b8b7a15
Malebestia <malebestia@yahoo.it>
2020-05-26 19:30:39 +0200

    .

--- Models/Home.php
+++ Models/Home.php
@@ -3,12 +3,13 @@
 namespace Modules\Blog\Models;
 
 use Carbon\Carbon;
-
+use Modules\Blog\Models\Traits\RatingTrait;
 //------services---------
 //--- TRAITS ---
 
 class Home extends BaseModelLang
 {
+    use RatingTrait;
     protected $fillable = ['post_id', 'article_type', 'icon_src',];
     //--------- relationship ---------------
 
