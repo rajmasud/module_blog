bc24231de1093c1142410bf9da6df7a942bbbfbd
Malebestia <malebestia@yahoo.it>
2020-06-18 22:54:31 +0200

    up

--- Models/Policies/PostPolicy.php
+++ Models/Policies/PostPolicy.php
@@ -2,14 +2,15 @@
 
 namespace Modules\Blog\Models\Policies;
 
+use Modules\Xot\Contracts\UserContract as User;
 use Modules\Xot\Models\Policies\XotBasePolicy;
 
 class PostPolicy extends XotBasePolicy {
-    public function deleteNoPostId($user, $post) {
+    public function deleteNoPostId(User $user, $post) {
         return true;
     }
 
-    public function clearDuplicates($user, $post) {
+    public function clearDuplicates(User $user, $post) {
         return true;
     }
 }
