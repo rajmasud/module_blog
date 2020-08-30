9bcf849e9050bcaef2270b7b965ee42483b24213
Malebestia <malebestia@yahoo.it>
2020-08-30 12:27:04 +0200

    .

--- Models/Panels/Actions/PersonalInfoAction.php
+++ Models/Panels/Actions/PersonalInfoAction.php
@@ -5,6 +5,7 @@ namespace Modules\Blog\Models\Panels\Actions;
 //-------- models -----------
 
 //-------- services --------
+use Illuminate\Support\Facades\Session;
 use Modules\Theme\Services\ThemeService;
 use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
 
@@ -17,6 +18,7 @@ class PersonalInfoAction extends XotBasePanelAction {
 
     public function handle() {
         $view = 'pub_theme::profile.'.$this->getName();
+        //dddx(get_defined_vars());
 
         return ThemeService::view($view)
             ->with('row', $this->row);
@@ -24,18 +26,27 @@ class PersonalInfoAction extends XotBasePanelAction {
 
     public function postHandle() {
         $data = request()->all();
-
+        //dddx($data['handle']);
+        /*
         \Validator::make($data, [
             //'name' => '',
             //'surname' => '',
-            'email' => 'required|email|unique:users',
-            'phone' => 'integer',
-            'bio' => 'max:200',
-        ])->validate();
+            //'email' => 'required|email|unique:users',
+            //'phone' => 'integer',
+            //'bio' => 'max:200',
+            ])->validate();
+        */
 
         //dddx($data);
         $profile = $this->row;
         $profile->update($data);
+        $profile->user->update(['handle' => $data['handle']]);
+
+        $swal = [
+            'icon' => 'success',
+            'title' => 'Aggiornamento riuscito',
+        ];
+        Session::flash('swal', $swal);
 
         return $this->handle();
     }
